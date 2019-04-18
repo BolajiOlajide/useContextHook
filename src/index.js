26e8cca87d1c9adf9452510e896fa1adb02736d5
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2019-04-18 01:39:15 +0100

    learn useRef hook

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
@@ -9,15 +9,34 @@ function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <Button />
+      <br />
+      <TextInputWithFocusButton />
     </div>
   );
 }
 
+// useContext hook
 const ThemeContext = React.createContext("light");
 
 function Button() {
   const theme = useContext(ThemeContext);
   return <button className={theme}>Amazing button</button>;
+}
+
+// useRef hook
+function TextInputWithFocusButton() {
+  const inputEl = useRef(null);
+  const onButtonClick = () => {
+    // `current` points to the mounted text input element
+    inputEl.current.focus();
+    inputEl.current.value = "Bolaji";
+  };
+  return (
+    <>
+      <input ref={inputEl} type="text" />
+      <button onClick={onButtonClick}>Focus the input</button>
+    </>
+  );
 }
 
 const rootElement = document.getElementById("root");
